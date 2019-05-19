e4d4b13801453d5d013a348fb14634fe693c0d64
Elisabeth <ortega.elisabeth@gmail.com>
2019-05-19 20:45:47 +0200

    Created a model with some properties of a molecule and added to admin panel

--- molecules/admin.py
+++ molecules/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Molecule
 # Register your models here.
+admin.site.register(Molecule)
--- molecules/models.py
+++ molecules/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Molecule(models.Model):
+    name = models.CharField(max_length=200)
+    mass = models.FloatField(default=0)
+    reference = models.CharField(max_length=200)
+    ccs = models.FloatField(default=0)
+    sdf_file = models.CharField(max_length=50000, default='none')
+
